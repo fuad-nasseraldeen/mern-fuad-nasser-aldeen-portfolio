29c414c8b8be1279872e4a70f1beedae79f1d0f1
unknown <fuadsami5@gmail.com>
2023-11-15 11:17:39 +0200

    fix server issue-7

--- server.js
+++ server.js
@@ -2,16 +2,11 @@ const express = require('express')
 const connectDB = require('./config/db') // call db file
 const path = require('path')
 
-const socketIO = require('socket.io')
-const http = require('http')
-
-
-const server = http.createServer(express)
-const io = socketIO(server)
+const app = express()
 // Connect DB
 connectDB()
 
-// //Init Middleware
+// Init Middleware
 app.use(express.json({ extended: false }))
 
 // app.get('/', (req, res) => res.send('API Running'))
@@ -23,18 +18,7 @@ app.use('/api/profile', require('./routes/api/profile'))
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'))
-  // Log messages from the server to the client
-  io.on('connection', (socket) => {
-    console.log('A client connected')
-    socket.on('disconnect', () => {
-      console.log('A client disconnected')
-    })
 
-    // Example server-side log
-    console.log('Sending a log message from the server')
-    socket.emit('serverLog', 'This message is from the server')
-    socket.emit('path', path)
-  })
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
